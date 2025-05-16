443062bb43dd8c1f00c245c7b63f8072ac840557
shambhavi-masade <shambhavimasade10@gmail.com>
2025-05-16 12:38:11 +0530

    Made Features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,30 +1,50 @@
-import React from 'react'
+import React from "react";
 import { FaTruckFast } from "react-icons/fa6";
 import { FaHeadset } from "react-icons/fa";
 import { RiShoppingBag3Line } from "react-icons/ri";
 import { FaBoxOpen } from "react-icons/fa";
+import "../CSS/Features.css";
 
 function Features() {
   return (
-    <div>
-      <div class="container text-center">
-  <div class="row align-items-center">
-    <div class="col">
-      <FaTruckFast />
+    <div className="features">
+      <div className="container text-center">
+        <div className="row align-items-center">
+          <div className="col">
+            <FaTruckFast className="Icons" />
+            <div className="feat-text">
+              <h5>Free Shipping</h5>
+              <p>Free Shipping on all your Orders</p>
+            </div>
+          </div>
+
+          <div className="col">
+            <FaHeadset className="Icons" />
+            <div className="feat-text">
+              <h5>Customer Support 24/7</h5>
+              <p>Instant access to support</p>
+            </div>
+          </div>
+
+          <div className="col">
+            <RiShoppingBag3Line className="Icons" />
+            <div className="feat-text">
+              <h5>100% Secure Payment</h5>
+              <p>We ensure your money is safe</p>
+            </div>
+          </div>
+
+          <div className="col">
+            <FaBoxOpen className="Icons" />
+            <div className="feat-text">
+              <h5>Money-Back Guarantee</h5>
+              <p>30 Days Money-Back Guarantee</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col">
-      <FaHeadset />
-    </div>
-    <div class="col">
-      <RiShoppingBag3Line />
-    </div>
-    <div class="col">
-      <FaBoxOpen />
-    </div>
-  </div>
-</div>
-    </div>
-  )
+  );
 }
 
-export default Features
+export default Features;
